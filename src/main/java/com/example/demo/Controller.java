fa3eb1b9252b49cfafd1b80e07fe2bcbc37e6188
sarmaria <88794424+sarmaria@users.noreply.github.com>
2022-11-16 20:07:30 +0100

    Update Controller.java

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String sayHello(){
         return "Hello World";
     }
