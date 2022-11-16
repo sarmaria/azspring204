7249d595977c14fb50c628078320cd57eb669ca7
charantechie <charantechie@gmail.com>
2022-11-16 17:37:14 +0100

    springbootwebapp

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String sayHello(){
         return "Hello World";
     }
